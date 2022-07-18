893622d0cde068039c6e6f57d1a58f5d6811226e
Michael Franco <franco.luis.michael@gmail.com>
2022-07-18 18:46:11 -0500

    commit tree 9680ecf3b5a9b88dbd313375720f9dd97579a8bb

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const scubersLocation = 42;
+const block = 264;
+
+
+function distanceFromHqInBlocks(pickupLocation) {
+    if(pickupLocation > scubersLocation) {
+        return pickupLocation - scubersLocation;
+    } else {
+        return scubersLocation - pickupLocation;
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * block;
+}
+
+function distanceTravelledInFeet(a, b) {
+    if (b > a) {
+        return (b-a)*264;
+    }else if (a > b) {
+        return(a-b)*264;
+    }
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    let n = distanceTravelledInFeet(start, destination);
+
+    if (n <= 400) {
+        return 0;
+    } else if (n <= 2000) {
+        return (n-400) * 0.02;
+    } else if (n <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+// calculatesFarePrice(34,32) 
+// calculatesFarePrice(50,58)
+// calculatesFarePrice(34,24) 
